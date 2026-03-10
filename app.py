e702ca2544e77a2774eb6ab3e812b77e6ebd019e
Luc Desforges <desforgesluc00@gmail.com>
2026-03-10 15:02:06 -0400

    implemented csv tools

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import tools
+import tools.csv_tools
+import numpy as np
 
-bmptools = tools.BMP_Tools()
-
-csvtools = tools.CSV_Tools()
+csvtools = tools.csv_tools.CSV_Tools()
+csvtools.export(np.array([1,2,3], dtype=np.uint8))
--- tools/csv_tools.py
+++ tools/csv_tools.py
@@ -3,13 +3,28 @@ import numpy as np
 from pathlib import Path
 
 class CSV_Tools:
-    INPUT_PATH = "../../input/csv"
+    PATH = ""
+    FILENAME = 'input.csv'
+    INPUT_PATH = "input/csv/"
+    EXTENSION_PATTERN = "*.csv"
 
     def __init__(self):
-        pass
+        self.PATH = Path(__file__).parent.parent
     
-    def read():
-        pass
+    def read(self):
+        path = Path(self.PATH / self.INPUT_PATH)
 
-    def export():
-        pass
+        try:
+            first_file = next(path.glob(self.EXTENSION_PATTERN))
+            print(f"File found with extension 'csv' with path: '{first_file}'")
+            return open(first_file).read()
+        except StopIteration:
+            print(f"No file found with extension 'csv' in '{path}'")
+            return None
+
+    def export(self, pixels: np.ndarray):
+        fixed_array = pixels.reshape(-1, pixels.shape[-1] if pixels.ndim == 3 else 1)
+        np.savetxt(Path(self.PATH / self.INPUT_PATH) / self.FILENAME, 
+                   fixed_array, 
+                   delimiter=",", 
+                   fmt='%d')
