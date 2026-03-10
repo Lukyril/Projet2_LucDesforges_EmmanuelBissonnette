1833c5e8f3ae46d7135e7648b408a867186f0bda
Luc Desforges <desforgesluc00@gmail.com>
2026-03-10 14:24:31 -0400

    read and export functionalities

--- tools/bitmap_tools.py
+++ tools/bitmap_tools.py
@@ -1,14 +1,34 @@
 from pathlib import Path
+import numpy as np
+from PIL import Image
 
 class BMP_Tools:
-    INPUT_PATH = "../../input/bmp"
-    OUTPUT_PATH = "../../output/bmp"
+    PATH = ""
+    OUTPUT_PATH = "output/bmp"
+    FILENAME = 'output_image.bmp'
+    INPUT_PATH = "input/bmp/"
+    EXTENSION_PATTERN = "*.bmp"
 
     def __init__(self):
-        pass
+        base_dir = Path(__file__).parent.parent
+        print(f"Base Directory: {base_dir}")
+        self.PATH = base_dir
 
     def read(self):
-        pass
+        
+        file_path = self.PATH / self.INPUT_PATH      
+        path = Path(file_path)
+        print(f"Dynamic File Path: {file_path}")
 
-    def export(self):
-        pass
+        try:
+            first_file = next(path.glob(self.EXTENSION_PATTERN))
+            print(f"File found with extension 'bmp' with path: '{first_file}'")
+            return open(first_file).read()
+        except StopIteration:
+            print(f"No file found with extension 'bmp' in '{path}'")
+            return None
+        
+    def export(self, pixels: np.ndarray):
+        path = Path(self.PATH / self.INPUT_PATH  )
+        image = Image.fromarray(pixels, mode='L')
+        image.save(path / self.FILENAME, format='BMP')
