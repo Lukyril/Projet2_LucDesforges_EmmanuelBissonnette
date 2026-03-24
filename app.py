d270eb09d18557f4e13b681c224de32d115dbad7
Luc Desforges <desforgesluc00@gmail.com>
2026-03-24 09:56:31 -0400

    brought changes from tools csv bmp

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import tools.bmp_csv_tools
 import numpy as np
 
+<<<<<<< Updated upstream
+=======
+csv_tool = tools.csv_tools.CSV_Tools()
+csv_tool.change_brightness_value(0, 255)
+>>>>>>> Stashed changes
 converter_tool = tools.bmp_csv_tools.BMP_CSV_Tools()
 converter_tool.convert_bmp_to_csv()
 converter_tool.convert_csv_to_bmp()
